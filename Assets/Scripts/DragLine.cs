c11a4e7fec078bd3ac9bc70e2cf766595b938214
Totokis <pawelj_janusz@wp.pl>
2021-04-08 12:18:23 +0200

    Last changes

---
+++ Assets/Scripts/DragLine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragLine : MonoBehaviour
+{
+    LineRenderer _lineRender;
+    Bird _bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRender = GetComponent<LineRenderer>();
+        _bird = FindObjectOfType<Bird>();
+
+        Vector3 lineZeroPostion = new Vector3(
+            _bird.transform.position.x,
+            _bird.transform.position.y,
+            -0.1f);
+        _lineRender.SetPosition(0,lineZeroPostion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_bird.IsDragging)
+        {
+            _lineRender.enabled = true;
+            _lineRender.SetPosition(1,_bird.transform.position);
+        }
+        else
+        {
+            _lineRender.enabled = false;
+        }
+       
+    }
+}
